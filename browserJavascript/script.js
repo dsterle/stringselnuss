e4ddbeb18763f716b0cc06ddc2463a38251dbdba
dsterle <dennis.sterle@smail.th-koeln.de>
2019-05-18 18:11:15 +0200

    headbar is fixed and first hyperlink brings you to the top of the page

--- browserJavascript/script.js
+++ browserJavascript/script.js
@@ -1,13 +1,21 @@
 const underScoreHeadbar = document.querySelector('.underScoreHeadbar');
 const headbarOnClicks = document.querySelectorAll('.headNav>div, .loginButton, .registrierungButton');
+const projektInfosButton = document.querySelector('.projektInfosButton');
 
 headbarOnClicks.forEach(function (item, index) {
     item.addEventListener("click", function () {
         /*underScoreHeadbar.style.width = (parseInt(getComputedStyle(item).width) + 41).toString() + 'px';
         underScoreHeadbar.style.left = (item.getBoundingClientRect().left).toString() + 'px';
         underScoreHeadbar.style.transition = '1s'*/
+        if (item.className === "projektInfosButton") {
+        }
+        if (item.className === "neueListeButton") {
+        }
+
         $(".underScoreHeadbar").css({"width":(parseInt(getComputedStyle(item).width) + parseInt(getComputedStyle(item).padding)*2+1).toString(),
                                         "left":(item.getBoundingClientRect().left).toString() + 'px',
                                         "transition":"1s"});
+        window.scrollTo({top: 0, behavior:"smooth"})
     })
-})
+});
+
