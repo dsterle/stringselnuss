ce91ad2d673484eede3b8d9572e011bd051a6402
dsterle <dennis.sterle@smail.th-koeln.de>
2019-05-26 22:48:51 +0200

    login- und registrierung-box hinzugefügt; language-div hinzugefügt

--- browserJavascript/script.js
+++ browserJavascript/script.js
@@ -2,12 +2,20 @@ const underScoreHeadbar = document.querySelector('.underScoreHeadbar');
 const headbarOnClicks = document.querySelectorAll('.headNav>div, .loginButton, .registrierungButton');
 const projektInfosButton = document.querySelector('.projektInfosButton');
 const scrollUpButton = document.querySelector('.scrollUp');
+const loginButton = document.querySelector(".loginButton");
+const loginBox = document.querySelector(".loginBox");
+const registrierungButton = document.querySelector(".registrierungButton");
+const registrierungBox = document.querySelector(".registrierungBox");
+const languageButton = document.querySelector(".language");
+
 
 $("section").hide();
 $(".scrollUp").hide();
 $(".projektInfos").show();
 
 var currentScrollPosition = window.pageYOffset;
+var loginClicked = false;
+var registrierungClicked = false;
 
 $(window).scroll(function() {
     currentScrollPosition = window.pageYOffset;
@@ -71,4 +79,85 @@ scrollUpButton.addEventListener("mouseout", function () {
     scrollUpButton.style.background = "rgb(179, 212, 255)";
     scrollUpButton.style.transition = "0.8s";
 });
+
+loginButton.addEventListener("click", function () {
+    if (loginClicked === false) {
+        if (registrierungClicked === true) {
+            registrierungBox.style.transform = "translateY(-100%)";
+            registrierungBox.style.transition = "0.8s";
+            registrierungClicked = false;
+        }
+        loginBox.style.transform = "translateY(0%)";
+        loginBox.style.transition = "0.8s";
+        loginClicked = true;
+    } else if (loginClicked === true){
+        loginBox.style.transform = "translateY(-100%)";
+        loginBox.style.transition = "0.8s";
+        loginClicked = false;
+    }
+});
+
+document.querySelectorAll(".loginform>input[type=submit]").forEach(function (item, index) {
+    item.addEventListener("mouseover", function () {
+        item.style.borderColor = "rgb(129, 182, 255)";
+        item.style.background = "rgb(129, 182, 255)";
+        item.style.cursor = "pointer";
+        item.style.transition = "0.8s";
+    });
+});
+
+document.querySelectorAll(".loginform>input[type=submit]").forEach(function (item, index) {
+    item.addEventListener("mouseout", function () {
+        item.style.background = "rgb(179, 212, 255)";
+        item.style.transition = "0.8s";
+    });
+});
+
+registrierungButton.addEventListener("click", function () {
+    if (registrierungClicked === false) {
+        if (loginClicked === true) {
+            loginBox.style.transform = "translateY(-100%)";
+            loginBox.style.transition = "0.8s";
+            loginClicked = false;
+        }
+        registrierungBox.style.transform = "translateY(0%)";
+        registrierungBox.style.transition = "0.8s";
+        registrierungClicked = true;
+    } else if (registrierungClicked === true){
+        registrierungBox.style.transform = "translateY(-100%)";
+        registrierungBox.style.transition = "0.8s";
+        registrierungClicked = false;
+    }
+});
+
+document.querySelectorAll(".registrierungform>input[type=submit]").forEach(function (item, index) {
+    item.addEventListener("mouseover", function () {
+        item.style.borderColor = "rgb(129, 182, 255)";
+        item.style.background = "rgb(129, 182, 255)";
+        item.style.cursor = "pointer";
+        item.style.transition = "0.8s";
+    });
+});
+
+document.querySelectorAll(".registrierungform>input[type=submit]").forEach(function (item, index) {
+    item.addEventListener("mouseout", function () {
+        item.style.background = "rgb(179, 212, 255)";
+        item.style.transition = "0.8s";
+    });
+});
+
+languageButton.addEventListener("mouseover", function () {
+    languageButton.style.borderColor = "rgb(129, 182, 255)";
+    languageButton.style.background = "rgb(129, 182, 255)";
+    languageButton.style.cursor = "pointer";
+    languageButton.style.transition = "0.8s";
+});
+
+languageButton.addEventListener("mouseout", function () {
+    languageButton.style.borderColor = "rgb(179, 212, 255)";
+    languageButton.style.background = "rgb(179, 212, 255)";
+    languageButton.style.transition = "0.8s";
+});
+
+
 
